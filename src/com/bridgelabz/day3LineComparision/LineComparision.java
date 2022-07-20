44fa35e9d881f63ec6cec07fb9427d2c737cc962
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-20 16:32:20 +0530

    [Shreyash]: added program to check equality of length of lines AB & CD

--- src/com/bridgelabz/day3LineComparision/LineComparision.java
+++ src/com/bridgelabz/day3LineComparision/LineComparision.java
@@ -18,15 +18,39 @@ public class LineComparision {
 		int x1 = scan.nextInt();
 		int y1 = scan.nextInt();
 		
-		System.out.println("Enter point B(x2,y2) :");
+		System.out.println(" Enter point B(x2,y2) :");
 		int x2 = scan.nextInt();
 		int y2 = scan.nextInt();
 		
 		
-		double lengthOfLine1 = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));	// calculating length of line
+		Double lengthOfLineAB = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));	// calculating length of line AB
 		
 		System.out.println("\n Two Points Are A(" +x1 +"," +y1 +") & B(" +x2 +"," +y2 +")");
-		System.out.println("Length of the line AB is " + lengthOfLine1);
+		System.out.println(" Length of the line AB is " + lengthOfLineAB);
+		
+		/*
+		 * 		taking inputs for p1,q1 & p2,q2
+		 */
+		
+		System.out.println("\n Enter point C(p1,q1) :");
+		int p1 = scan.nextInt();
+		int q1 = scan.nextInt();
+		
+		System.out.println(" Enter point D(p2,q2) :");
+		int p2 = scan.nextInt();
+		int q2 = scan.nextInt();
+		
+		Double lengthOfLineCD = Math.sqrt((p2-p1) * (p2-p1) + (q2-q1) * (q2-q1));	// calculating length of line CD
+		
+		System.out.println("\n Two Points Are C(" +p1 +"," +q1 +") & D(" +p2 +"," +q2 +")");
+		System.out.println(" Length of the line CD is " + lengthOfLineCD);
+	    
+		/*
+		 * 		checking equality of length of lines AB & CD using equals method
+		 */
+		
+	    System.out.println("\n Length of lines AB & CD are equal : " + lengthOfLineAB.equals(lengthOfLineCD) );
+
 		
 scan.close();
 		
