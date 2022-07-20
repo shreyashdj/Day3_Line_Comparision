83933d056e5faf4c67e5cd8824df153b63ac6f1c
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-20 14:21:58 +0530

    [Shreyash]: added program to print welcome message

---
+++ src/com/bridgelabz/day3LineComparision/LineComparision.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day3LineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Line Comparision");
+		
+	}
+
+}
